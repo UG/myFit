3b1628c0b204057cbd449f0ec7432a55b4105136
Yuji Noguchi <yuji.noguchi@sp7-mac-0012.local>
2021-09-15 17:15:38 +0900

    async applied

--- creatingdb/createdb.js
+++ creatingdb/createdb.js
@@ -1,49 +1,71 @@
 const sqlite3 = require("sqlite3");
+const { open } = require("sqlite");
 const fs = require("fs");
 const csv = require("csv");
-const db = new sqlite3.Database("./Nutrition.db");
-const file = "./nutrition.csv";
+const { resolve } = require("path");
+const { count } = require("console");
+const dbFile = "./Nutrition.db";
 
-const createDB = async () => {
-  const createTable = `
-create table if not exists Food(
-  id Text unique not null primary key,
-  name Text,
-  calories real,
-  protein real,
-  fat real,
-  carbo real,
-  fiber real
-)
-`;
-  db.run(createTable);
-
-  const parser = csv.parse((error, data) => {
-    for (let a = 0; a < data.length; a++) {
-      const elem = data[a];
-      if (a > 2) {
-        const insertArray = new Array(
-          `'${elem[0]}'`,
-          `'${elem[2]}'`,
-          parseFloat(elem[4].toString().replace(/-|Tr/i, "0.0")),
-          parseFloat(elem[7].toString().replace(/-|Tr/i, "0.0")),
-          parseFloat(elem[10].toString().replace(/-|Tr/i, "0.0")),
-          parseFloat(elem[11].toString().replace(/-|Tr/i, "0.0")),
-          parseFloat(elem[14].toString().replace(/-|Tr/i, "0.0"))
-        );
-        const upsert = `insert into Food(id,name,calories,protein,fat,carbo,fiber) values(${insertArray.join(
-          ","
-        )}) on conflict(id) do nothing`;
-        //console.log(upsert);
-        db.run(upsert);
-      }
-    }
-    db.get(`select count(id) from Food`, (err, row) => {
-      console.log(row);
-    });
+async function createFoodTable(db) {
+  return new Promise(async (resolve) => {
+    const sql = `
+    create table if not exists Food(
+    id Text unique not null primary key,
+    name Text,
+    calories real,
+    protein real,
+    fat real,
+    carbo real,
+    fiber real
+    );`;
+    await db.exec(sql);
+    return resolve("complete creating table");
   });
-  fs.createReadStream(file).pipe(parser);
-  console.log("complete");
+}
+
+async function insertFood(db) {
+  const file = "./nutrition.csv";
+  return new Promise(async (resolve) => {
+    const parser = csv.parse(async (error, data) => {
+      for (let a = 0; a < data.length; a++) {
+        const elem = data[a];
+        if (a > 2) {
+          const insertArray = new Array(
+            `'${elem[0]}'`,
+            `'${elem[2]}'`,
+            parseFloat(elem[4].toString().replace(/-|Tr/i, "0.0")),
+            parseFloat(elem[7].toString().replace(/-|Tr/i, "0.0")),
+            parseFloat(elem[10].toString().replace(/-|Tr/i, "0.0")),
+            parseFloat(elem[11].toString().replace(/-|Tr/i, "0.0")),
+            parseFloat(elem[14].toString().replace(/-|Tr/i, "0.0"))
+          );
+          const upsert = `insert into Food(id,name,calories,protein,fat,carbo,fiber) values(${insertArray.join(
+            ","
+          )}) on conflict(id) do nothing`;
+          await db.run(upsert);
+        }
+      }
+      return resolve("insert complete");
+    });
+    fs.createReadStream(file).pipe(parser);
+  });
+}
+
+async function countFoodRows(db) {
+  return new Promise(async (resolve) => {
+    const result = await db.get(`select count(id) from Food`);
+    return resolve(result);
+  });
+}
+
+const createFood = async () => {
+  const db = await open({
+    filename: dbFile,
+    driver: sqlite3.Database,
+  });
+  console.log(await createFoodTable(db));
+  console.log(await insertFood(db));
+  console.log(await countFoodRows(db));
 };
 
-createDB();
+createFood();
